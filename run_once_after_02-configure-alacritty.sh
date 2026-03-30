ab0570be9ef197b305b8dfea7b724e220386dd49
Julien Klaer <j.klaer@tadaweb.com>
2026-03-31 00:39:47 +0200

    fix(alacritty): use official doc to update desktop database

--- run_once_after_02-configure-alacritty.sh
+++ run_once_after_02-configure-alacritty.sh
@@ -2,17 +2,18 @@
 set -euo pipefail
 
 ALACRITTY_REPO="https://raw.githubusercontent.com/alacritty/alacritty/master"
+TMPDIR=$(mktemp -d)
+trap "rm -rf $TMPDIR" EXIT
 
 echo "==> Installing Alacritty terminfo..."
-curl -sSL "${ALACRITTY_REPO}/extra/alacritty.info" | tic -xe alacritty,alacritty-direct -
-
-echo "==> Installing Alacritty desktop entry..."
-mkdir -p ~/.local/share/applications
-curl -sSL "${ALACRITTY_REPO}/extra/linux/Alacritty.desktop" -o ~/.local/share/applications/Alacritty.desktop
+curl -sSL "${ALACRITTY_REPO}/extra/alacritty.info" -o "$TMPDIR/alacritty.info"
+sudo tic -xe alacritty,alacritty-direct "$TMPDIR/alacritty.info"
 
 echo "==> Installing Alacritty icon..."
-mkdir -p ~/.local/share/icons
-curl -sSL "${ALACRITTY_REPO}/extra/logo/alacritty-term.svg" -o ~/.local/share/icons/Alacritty.svg
+curl -sSL "${ALACRITTY_REPO}/extra/logo/alacritty-term.svg" -o "$TMPDIR/Alacritty.svg"
+sudo cp "$TMPDIR/Alacritty.svg" /usr/share/pixmaps/Alacritty.svg
 
-echo "==> Updating desktop database..."
-update-desktop-database ~/.local/share/applications
+echo "==> Installing Alacritty desktop entry..."
+curl -sSL "${ALACRITTY_REPO}/extra/linux/Alacritty.desktop" -o "$TMPDIR/Alacritty.desktop"
+sudo desktop-file-install "$TMPDIR/Alacritty.desktop"
+sudo update-desktop-database
